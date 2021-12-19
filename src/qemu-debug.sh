fbe76aecb80bb4ded86f3830106ee46671d5a7f8
wtperry <wtperry@umich.edu>
2021-12-19 12:27:08 -0600

    delete unused scripts

--- src/qemu-debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-. ./iso.sh
- 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -s -S -cdrom labrador.iso &
-gdb -ex 'target remote localhost:1234' -ex 'symbol-file kernel/labrador.kernel'
