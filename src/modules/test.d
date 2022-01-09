24e1b5fe72370204426e7d1688862482a4e4d899
wtperry <wtperry@umich.edu>
2022-01-09 09:16:52 -0600

    start of modules

---
+++ src/modules/test.d
@@ -0,0 +1,4 @@
+test.o: test.c ../sysroot/usr/include/stdio.h \
+ ../sysroot/usr/include/sys/cdefs.h \
+ /home/wtperry/opt/cross/lib/gcc/x86_64-elf/11.1.0/include/stdarg.h \
+ /home/wtperry/opt/cross/lib/gcc/x86_64-elf/11.1.0/include/stddef.h
