b5c5a4d5a338b604054fadb4bad022e064618b87
wtperry <wtperry@umich.edu>
2022-01-15 13:55:16 -0600

    memcpy optimization?

---
+++ src/libc/arch/x86_64/memcpy.c
@@ -0,0 +1,10 @@
+#include <string.h>
+ 
+void* memcpy(void* restrict dst, const void* restrict src, size_t size) {
+	asm volatile( "cld; rep movsb"
+                : "=c"((int){0})
+                : "D"(dst), "S"(src), "c"(size)
+                : "flags", "memory");
+    
+    return dst;
+}
