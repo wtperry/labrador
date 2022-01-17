2c1003350e6079d736115d8a83afcead59171e8e
wtperry <wtperry@umich.edu>
2022-01-16 20:14:45 -0600

    hpet tables

--- src/kernel/arch/x86_64/include/kernel/arch/acpi.h
+++ src/kernel/arch/x86_64/include/kernel/arch/acpi.h
@@ -73,3 +73,25 @@ struct madt_entry_2 {
     uint32_t int_num;
     uint16_t flags;
 } __attribute__((packed));
+
+struct address_struct {
+    uint8_t address_space_id;
+    uint8_t register_bit_width;
+    uint8_t register_bit_offset;
+    uint8_t reserved;
+    uint64_t address;
+} __attribute__((packed));
+
+struct hpet {
+    struct sdt_hdr hdr;
+    uint8_t hardware_rev_id;
+    uint8_t comparator_count:5;
+    uint8_t counter_size:1;
+    uint8_t reserved:1;
+    uint8_t legacy_replacement:1;
+    uint16_t pci_vendor_id;
+    struct address_struct address;
+    uint8_t hpet_number;
+    uint16_t minimum_tick;
+    uint8_t page_protection;
+} __attribute__((packed));
